75a72ae6f33fbff578df0b11678c4a29573f491c
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-25 14:08:27 +0200

    finish tests about Door module

--- portal/test/portal/door_test.exs
+++ portal/test/portal/door_test.exs
@@ -6,4 +6,31 @@ defmodule DoorTest do
     {val, _}  = Portal.Door.start_link(:green)
     assert val == :ok
   end
+
+  test "should return the list contained in a door" do
+    Portal.Door.start_link(:pink)
+    assert Portal.Door.get(:pink) == []
+
+    Portal.Door.push(:pink, 25)
+    assert Portal.Door.get(:pink) == [25]
+  end
+
+  test "should add an item for a door" do
+    Portal.Door.start_link(:pink)
+    assert Portal.Door.get(:pink) == []
+
+    Portal.Door.push(:pink, 25)
+    assert Portal.Door.get(:pink) == [25]
+  end
+
+  test "should pop an item for a door" do
+    Portal.Door.start_link(:pink)
+    assert Portal.Door.get(:pink) == []
+
+    Portal.Door.push(:pink, 25)
+    assert Portal.Door.get(:pink) == [25]
+
+    assert Portal.Door.pop(:pink) == {:ok, 25}
+    assert Portal.Door.get(:pink) == []
+  end
 end
