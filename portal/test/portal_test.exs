234e22d7382a9ac0ea4479a95f23a4685a2dde27
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-25 19:17:25 +0200

    add some tests not finish

--- portal/test/portal_test.exs
+++ portal/test/portal_test.exs
@@ -2,7 +2,33 @@ defmodule PortalTest do
   use ExUnit.Case
   doctest Portal
 
-  test "greets the world" do
-    assert Portal.hello() == :world
+  test "create a portal" do
+    Portal.Door.start_link(:orange)
+    Portal.Door.start_link(:blue)
+    portal = Portal.transfer(:orange, :blue, [1, 2, 3])
+   # IO.puts portal
+    #assert portal == {left: :orange, right: :blue}
   end
+
+  test "test transfer of data" do
+    Portal.Door.start_link(:orange)
+    Portal.Door.start_link(:blue)
+    portal = Portal.transfer(:orange, :blue, [1, 2, 3])
+    orange_data = Portal.Door.get(:orange);
+    blue_data = Portal.Door.get(:blue)
+    assert orange_data = [3, 2, 1]
+    assert blue_data = []
+  end
+
+  test "push right one the data from the left to the right" do
+    Portal.Door.start_link(:orange)
+    Portal.Door.start_link(:blue)
+    portal = Portal.transfer(:orange, :blue, [1, 2, 3])
+    Portal.push_right(portal)
+    orange_data = Portal.Door.get(:orange);
+    blue_data = Portal.Door.get(:blue)
+    assert orange_data = [2, 1]
+    assert blue_data = [3]
+  end
+
 end
