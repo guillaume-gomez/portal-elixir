8e604731c2a07271a48017b0936aaf07841cccab
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-27 18:33:38 +0200

    add child supervisor

--- portal/lib/portal/application.ex
+++ portal/lib/portal/application.ex
@@ -8,8 +8,7 @@ defmodule Portal.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Portal.Worker.start_link(arg)
-      # {Portal.Worker, arg},
+      worker(Portal.Door, [])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
