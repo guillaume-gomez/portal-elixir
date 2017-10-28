8f9973436cb8ae26d9c34485908e497743134fd3
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-29 00:02:53 +0200

    import missing lib"

--- portal/lib/portal/application.ex
+++ portal/lib/portal/application.ex
@@ -6,12 +6,10 @@ defmodule Portal.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      worker(Portal.Door, [])
-    ]
+    import Supervisor.Spec
+    children = [worker(Portal.Door, [])]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
